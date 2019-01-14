69b6c3ddeb2dc861e79bed0fc1dd37d95bab3ef6
Keoni Murray <keonimurray45@gmail.com>
2019-01-13 23:25:52 -0800

    added comments

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+//importing dependencies
 const figlet = require('figlet');
 const chalk = require('chalk');
 const args = process.argv;
@@ -10,13 +11,16 @@ const turl = require('turl');
 const adapter = new FileSync('db.json')
 const db = low(adapter)
 
+//list of commands that will be accepted
 const commands = ['new', 'get', 'remove', 'help'];
 
 db.defaults({
+    //creates a array named web_link that will store user inputs 
     web_links: []
 }).write()
 
 const prompt = question => {
+    //This function will allows us to send messages from the terminal and accept input
     const r = rl.createInterface({
         input: process.stdin,
         output: process.stdout,
@@ -24,6 +28,7 @@ const prompt = question => {
     });
 
     return new Promise((resolve, error) => {
+        //error handler
         r.question(question, answer => {
             r.close()
             resolve(answer)
@@ -31,100 +36,75 @@ const prompt = question => {
     });
 }
 
-
-
-
-
 console.log(figlet.textSync('Keylink', {
+    //Display Keylink so user knows they are using the app
     font: 'Dr Pepper', //Doom'
     horizontalLayout: 'default',
     verticalLayout: 'default'
 }));
 
-
-
 const newLink = () => {
+    //Will accpet link and turn it into a short link then pass it to the add name function
     const link_question = chalk.blue('Please enter a link\n');
     prompt(link_question).then(link => {
-
+        //takes link_question and passed the value to link
+        //link value is passed to turl.shorten to turn link user gave to short link
         let shortLink = turl.shorten(link).then((res) => {
-
+            //short link saved in res the reassigned to shortLink variable
             shortLink = res;
-
-
+            //shortLink value passed to addname() function
             return addname(shortLink)
-
-
-
-
         }).catch((err) => {
+            //error handler
             console.log(err);
+
         });
-
-
-
-
-
     })
-
 }
 
 const addname = (linkName) => {
 
+    //this function saves name and link to file
     let name = chalk.blue('please give your link a name example github for github.com\n');
-
-
-
+    //Ask user give link a name
     prompt(name).then(newName => {
-
+        //passes link name user gsve to newName
         db.get('web_links').push({
+            //call the array inside the file and link and link name to it
             link: linkName,
             name: newName.replace(/\s/g, '')
-
-
         }).write()
-
+        //Tells user link and name has been saved
         console.log(chalk.green("link and name saved!"));
-
-
     })
 
 
 }
 
 const getLink = () => {
+    //Function get's links and names saved to file
     const linksList = db.get('web_links').value()
     let index = 1;
-
     linksList.forEach(link => {
+        //loops over links in list and prints them to user with index
         let linktext = `${index++}. ${link.name}: ${link.link}`
         console.log(chalk.green(linktext));
-
     });
 }
 
 const remove = () => {
     // This will remove the link the user saved
     let question = chalk.blue("What is the name of the link you want to remove?\n");
-
-
-
-
     prompt(question).then(removeLink => {
         db.get('web_links').remove({
             name: removeLink.replace(/\s/g, '')
-
         }).write()
-
+        console.log(chalk.green("link and name have been deleted!"));
     })
-
-
-
-
 }
 
 const help = () => {
-
+    //print info and how to use app to the user
     const usageText = `keylink is a tools that lets you save links as short links in
     a json file that you can read and write to. 
 
@@ -138,12 +118,12 @@ const help = () => {
     help:     used to print the usage guide
 
     `
-
     console.log(usageText);
 }
 
 
 const errorLog = error => {
+    //error handler
     const elog = chalk.red(error);
     console.log(elog);
 }
@@ -173,6 +153,4 @@ switch (args[2]) {
     default:
         errorLog(`only one argument can be accepted`);
         help()
-
-
 }
